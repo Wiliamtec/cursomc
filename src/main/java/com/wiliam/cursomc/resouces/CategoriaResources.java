6883708deb2a44d90a52b733eb39409b8bc12fc6
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-12 15:53:27 -0300

    Rest Testado

---
+++ src/main/java/com/wiliam/cursomc/resouces/CategoriaResources.java
@@ -0,0 +1,16 @@
+package com.wiliam.cursomc.resouces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResources {
+
+    @GetMapping
+    public String listar(){
+        return "Rest esta funcioando";
+    }
+
+}
