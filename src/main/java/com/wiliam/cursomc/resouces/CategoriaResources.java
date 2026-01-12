c39a1f76bebaefa0d52d0555447c3c467ae286dc
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-12 16:41:10 -0300

    Testando a primeira Classe de Dominio

--- src/main/java/com/wiliam/cursomc/resouces/CategoriaResources.java
+++ src/main/java/com/wiliam/cursomc/resouces/CategoriaResources.java
@@ -1,16 +1,28 @@
 package com.wiliam.cursomc.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wiliam.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
     @GetMapping
-    public String listar(){
-        return "Rest esta funcioando";
+    public List<Categoria> listar(){
+
+        Categoria c1 = new Categoria(01, "Escritorio");
+        Categoria c2 =new Categoria(02,"informatica");
+
+        List <Categoria> lista = new ArrayList<>();
+        lista.add(c2);
+        lista.add(c1);
+        return lista;
     }
 
 }
