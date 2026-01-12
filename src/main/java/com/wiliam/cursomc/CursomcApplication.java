e609a26623279d5bea047931794ea990399c37fa
Wiliam Reis <wiliamsup83@gmail.com>
2026-01-12 20:54:59 -0300

    Criando operação de instanciação

--- src/main/java/com/wiliam/cursomc/CursomcApplication.java
+++ src/main/java/com/wiliam/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.wiliam.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.wiliam.cursomc.domain.Categoria;
+import com.wiliam.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository repo;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null,"Informatica");
+		Categoria c2 = new Categoria(null,"Ecritorio");
+		repo.saveAll(Arrays.asList(c1,c2));
 	}
 
 }
